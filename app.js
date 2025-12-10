8f47148f8bf278fd3137500806eb3f2b6be69fdf
SDTOOLS <sahil09patil@gmail.com>
2025-12-11 00:30:33 +0530

    fix

--- app.js
+++ app.js
@@ -26,4 +26,5 @@ app.get("/login", (req, res) => res.render("Login"));
 app.get("/products", (req, res) => res.render("Products"));
 app.get("/services", (req, res) => res.render("Services"));
 
-app.listen(3000, () => console.log("Server running at http://localhost:3000"));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
